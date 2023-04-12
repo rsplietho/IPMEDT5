da3810ced3e8827e92ab57c53dfb46b4a8ad8a75
tomlommerse <tomlommerse2001@gmail.com>
2023-04-12 14:32:28 +0200

    grid toegevoegd

--- lichtkrant-controller/resources/views/components/preview.blade.php
+++ lichtkrant-controller/resources/views/components/preview.blade.php
@@ -1,6 +1,4 @@
 <div class="preview">
-    <br>
-    <p style="padding: 1rem; width: 95% ; background-color: black; color: #{{App\Http\Controllers\PreviewController::previewColour()}}; font-family: 'led_dot_matrix', sans-serif;">{{App\Http\Controllers\PreviewController::previewText()}}</p>
-    <br>
+    <p>{{App\Http\Controllers\PreviewController::previewText()}}</p>
 </div>
 
