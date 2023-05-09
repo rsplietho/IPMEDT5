601ec2ce9be908512c452d887de25f90a6a6066f
Perry J.R. Rijaard <93517178+Ch5pper@users.noreply.github.com>
2023-05-09 14:23:05 +0200

    new styling register page

--- lichtkrant-controller/resources/views/auth/register.blade.php
+++ lichtkrant-controller/resources/views/auth/register.blade.php
@@ -1,66 +1,95 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register Page</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+		body {
+			font-family: Arial, sans-serif;
+            background-image: url("https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701862770.jpg");
+		}
+		.container {
+			margin: 0 auto;
+			max-width: 400px;
+			padding: 20px;
+			background-color: #fff;
+			border-radius: 5px;
+			box-shadow: 0px 0px 5px 0px rgba(0,0,0,0.5);
+		}
+		h2 {
+			text-align: center;
+			color: #333;
+		}
+        h1 {
+			text-align: center;
+			color: white;
+            text-shadow: 2px 2px black;
+		}
+		label {
+			display: block;
+			margin-bottom: 5px;
+			color: #666;
+		}
+		input[type=text], input[type=password] {
+			width: 100%;
+			padding: 10px;
+			border: 1px solid #ccc;
+			border-radius: 5px;
+			margin-bottom: 15px;
+			box-sizing: border-box;
+		}
+		button {
+			background-color: #15616D;
+			color: #fff;
+			padding: 10px 15px;
+			border: none;
+			border-radius: 5px;
+			cursor: pointer;
+			width: 100%;
+		}
+		button:hover {
+			background-color: white;
+            color: #15616D;
+            border: 2px solid #15616D;
+		}
+		@media only screen and (max-width: 600px) {
+			.container {
+				max-width: 300px;
+			}
+		}
+	</style>
+</head>
+<body>
 
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+<h1>Lichtkrant</h1>
 
+	<div class="container">
+		<h2>Register</h2>
         <form method="POST" action="{{ route('register') }}">
-            @csrf
+			<label for="username" :value="__('Name')">Name</label>
+			<input id="name"  type="text" name="name" :value="old('name')" required autofocus />
 
-            <!-- Name -->
-            <div>
-                <x-label for="name" :value="__('Name')" />
+			<label for="password" :value="__('Username')" >Username</label>
+			<input id="username"  type="text" name="username" :value="old('username')" required />
+            
+            <label for="email" :value="__('Email')">E-mail</label>
+			<input id="email"  type="text" name="email" :value="old('email')" required />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
-            </div>
+            <label for="password" :value="__('Password')">Password</label>
+			<input id="password" 
+			type="password"
+			name="password"
+			required autocomplete="new-password" />
 
-            <!-- Username -->
-            <div class="mt-4">
-                <x-label for="username" :value="__('Username')" />
+			<label for="password" :value="__('Password')">Password confirmation</label>
+			<input id="password_confirmation" 
+			type="password"
+			name="password_confirmation" required />
 
-                <x-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required />
-            </div>
-
-            <!-- Email Address -->
-            <div class="mt-4">
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
-            </div>
-
-            <!-- Confirm Password -->
-            <div class="mt-4">
-                <x-label for="password_confirmation" :value="__('Confirm Password')" />
-
-                <x-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-button class="ml-4">
-                    {{ __('Register') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+			<button type="submit">                    
+				{{ __('Register') }}
+			</button>
+		</form>
+	</div>
+</body>
+</html>
--- lichtkrant-controller/routes/auth.php
+++ lichtkrant-controller/routes/auth.php
@@ -11,12 +11,12 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    // Registering disabled to prevent malicious use
+     //Registering disabled to prevent malicious use
 
-    // Route::get('register', [RegisteredUserController::class, 'create'])
-    //             ->name('register');
+     Route::get('register', [RegisteredUserController::class, 'create'])
+                 ->name('register');
 
-    // Route::post('register', [RegisteredUserController::class, 'store']);
+     Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
